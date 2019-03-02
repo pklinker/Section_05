d9223614076d13a1315e1439719235f3d81181bb
Paul Klinker <pklinker@gmail.com>
2019-03-02 10:03:25 -0500

    Reorganize folders - phase 4.

--- Source/Section_05/Section_05GameMode.cpp
+++ Source/Section_05/Section_05GameMode.cpp
@@ -9,7 +9,7 @@ ASection_05GameMode::ASection_05GameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behavior/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
--- Source/Section_05/Section_05HUD.cpp
+++ Source/Section_05/Section_05HUD.cpp
@@ -10,7 +10,7 @@
 ASection_05HUD::ASection_05HUD()
 {
 	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/FirstPerson/Textures/FirstPersonCrosshair"));
+	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/Player/Textures/FirstPersonCrosshair"));
 	CrosshairTex = CrosshairTexObj.Object;
 }
 
