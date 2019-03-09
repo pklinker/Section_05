12aa83d325f45ddc1847706f4a0e92339a92618b
Paul Klinker <pklinker@gmail.com>
2019-03-09 09:40:37 -0500

    Added a rifle. It is not yet integrated into the NPCs or first person yet.

---
+++ Source/Section_05/Weapon/Rifle.cpp
@@ -0,0 +1,78 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Rifle.h"
+#include "Kismet/GameplayStatics.h"
+#include "Weapon/FirstPersonProjectile.h"
+#include "Animation/AnimInstance.h"
+
+// Sets default values
+ARifle::ARifle()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	// Create a gun mesh component
+	RifleComponent = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FP_Gun"));
+	RifleComponent->SetOnlyOwnerSee(true);			// only the owning player will see this mesh
+	RifleComponent->bCastDynamicShadow = false;
+	RifleComponent->CastShadow = false;
+	// RifleComponent->SetupAttachment(Mesh1P, TEXT("GripPoint"));
+	RifleComponent->SetupAttachment(RootComponent);
+
+	MuzzleLocation = CreateDefaultSubobject<USceneComponent>(TEXT("MuzzleLocation"));
+	MuzzleLocation->SetupAttachment(RifleComponent);
+	MuzzleLocation->SetRelativeLocation(FVector(0.2f, 48.4f, -10.6f));
+}
+
+// Called when the game starts or when spawned
+void ARifle::BeginPlay()
+{
+	Super::BeginPlay();
+	//Attach gun mesh component to Skeleton, doing it here because the skeleton is not yet created in the constructor
+//	RifleComponent->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
+
+}
+
+// Called every frame
+void ARifle::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ARifle::OnFire()
+{
+	// try and fire a projectile
+	if (ProjectileClass != NULL)
+	{
+		UWorld* const World = GetWorld();
+		if (World != NULL)
+		{
+			//const FRotator SpawnRotation = GetActorRotation();
+			const FRotator SpawnRotation = MuzzleLocation->GetComponentRotation();
+			// MuzzleOffset is in camera space, so transform it to world space before offsetting from the character location to find the final muzzle position
+			const FVector SpawnLocation =  MuzzleLocation->GetComponentLocation();
+
+			//Set Spawn Collision Handling Override
+			FActorSpawnParameters ActorSpawnParams;
+			ActorSpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButDontSpawnIfColliding;
+
+			// spawn the projectile at the muzzle
+			World->SpawnActor<AFirstPersonProjectile>(ProjectileClass, SpawnLocation, SpawnRotation, ActorSpawnParams);
+		}
+	}
+
+	// try and play the sound if specified
+	if (FireSound != NULL)
+	{
+		UGameplayStatics::PlaySoundAtLocation(this, FireSound, GetActorLocation());
+	}
+
+	// try and play a firing animation if specified
+	if (FireAnimation != NULL)
+	{
+		if (AnimInstance != NULL)
+		{
+			AnimInstance->Montage_Play(FireAnimation, 1.f);
+		}
+	}
+}
---
+++ Source/Section_05/Weapon/Rifle.h
@@ -0,0 +1,56 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Rifle.generated.h"
+
+UCLASS()
+class SECTION_05_API ARifle : public AActor
+{
+	GENERATED_BODY()
+	
+	/** Gun mesh: 1st person view (seen only by self) */
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+	class USkeletalMeshComponent* RifleComponent;
+
+	/** Location on gun mesh where projectiles should spawn. */
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+	class USceneComponent* MuzzleLocation;
+
+public:	
+	// Sets default values for this actor's properties
+	ARifle();
+	/** Gun muzzle's offset from the characters location */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+		FVector GunOffset;
+
+	/** Projectile class to spawn */
+	UPROPERTY(EditDefaultsOnly, Category = Projectile)
+		TSubclassOf<class AFirstPersonProjectile> ProjectileClass;
+
+	/** Sound to play each time we fire */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+		class USoundBase* FireSound;
+
+	/** AnimMontage to play each time we fire */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+		class UAnimMontage* FireAnimation;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+		class UAnimInstance* AnimInstance;
+
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	/** Fires a projectile. */
+	void OnFire();
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
