19a1c2484e561b0463173ad4d0c64bd1544da05a
Paul Klinker <pklinker@gmail.com>
2019-03-30 21:17:32 -0400

    Refactor NPC to character so both NPC and player are using the character folder now. FIxed C++ reference to PlayerCharacter to Character.

--- Source/Section_05/Section_05GameMode.cpp
+++ Source/Section_05/Section_05GameMode.cpp
@@ -9,7 +9,7 @@ ASection_05GameMode::ASection_05GameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Player/Behavior/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Character/Behavior/Character"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
