8ca665dea5aa4085cb9efb1156298a0b996ae2e5
Paul Klinker <pklinker@gmail.com>
2019-03-31 21:20:00 -0400

    Rename some classes according to UE4 Style Guide. Fixed passing through barrier by adding another trigger volume.

--- Source/Section_05/Section_05GameMode.cpp
+++ Source/Section_05/Section_05GameMode.cpp
@@ -9,7 +9,7 @@ ASection_05GameMode::ASection_05GameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Character/Behavior/Character"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Character/Behavior/BP_Character"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
